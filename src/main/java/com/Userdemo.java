fe195261e0f7933b3ba88ca01433a623229c5f29
mhx1999 <1282488032@qq.com>
2023-04-25 23:04:12 +0800

    first commit

---
+++ src/main/java/com/Userdemo.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Userdemo {
+   public void testdemo(){
+      User user =  new User("mhx",23);
+      System.out.println(user.getName().equals("mhx"));
+
+   }
+
+
+}
---
+++ src/test/java/com/test001.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.Test;
+
+public class test001 {
+    @Test
+        public void test(){
+            User user =  new User("mhx",23);
+            System.out.println(user.getName().equals("mhx"));
+
+        }
+
+}
